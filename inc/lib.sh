a952f79a9222782e08fb4ce03583afab0656365c
Héctor Menéndez <etor@gik.mx>
2015-02-08 02:25:42 -0600

    Updated install script so it can work on Linux.

---
+++ inc/lib.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function isLinux {
+    if [ "$(uname)" == "Darwin" ]; then
+        return 1 # Yeah, this is false
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+        return 0 # You guessed it, this is true
+    else
+        echo "The current OS is not supported"
+        exit 1
+    fi
+}
+
+function dotfiles {
+    local depth='depth'
+    if $(isLinux); then depth="max$depth"; fi
+    echo `find $1 -$depth 1 -type f -name '*.sh' -exec basename {} .sh \;`
+}
