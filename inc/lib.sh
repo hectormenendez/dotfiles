fc1c486bab674308ebc6edfa43e005c818a4c4dc
Héctor Menéndez <etor@gik.mx>
2015-02-08 03:56:24 -0600

    more validations for linux (rbenv and virtualenvrapper)

--- inc/lib.sh
+++ inc/lib.sh
@@ -2,13 +2,17 @@
 
 function isLinux {
     if [ "$(uname)" == "Darwin" ]; then
-        return 1 # Yeah, this is false
+        return 1 # 1? false!, yup, this is false
     elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
         return 0 # You guessed it, this is true
     else
         echo "The current OS is not supported"
         exit 1
     fi
+}
+
+function has {
+    if `command -v $1 > /dev/null 2>&1`; then return 0; else return 1; fi
 }
 
 function dotfiles {
