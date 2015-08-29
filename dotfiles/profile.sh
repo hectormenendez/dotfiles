fa20d10df5205710ec5e0131130ac7a7cd1904be
Héctor Menéndez <etor@gik.mx>
2015-08-29 05:12:59 -0500

    Added bash_completions (finally) and improved installation.

--- dotfiles/profile.sh
+++ dotfiles/profile.sh
@@ -22,28 +22,34 @@ if ! $(isLinux); then
 	export PATH=$(brew --prefix coreutils)/libexec/gnubin:$HOME/.nodebrew/current/bin:$PATH
 fi
 
-# If an alias file exists, load it.
-if [ -f ~/.alias ]; then source ~/.alias; fi
-
-# Only valid for Bash4: wildcard ** now means recursive
-shopt -s globstar
-
 # Set the creation mask, so files are created with 600 and dirs as 700
 umask 077
+
+# Shell options
+shopt -s checkwinsize # check winsize after each command and adjust screen automatically,
+shopt -s autocd       # Enable globs on cd
+shopt -s globstar     # wildcard ** now means recursive
+shopt -s nocaseglob   # case insensitive globbing
+shopt -s cdspell      # Autocorrect typos in path names when using cd
 
 # History management:
 # cleanup the history file, by manually removing dups
 tac $HISTFILE | awk '!x[$0]++' | tac | sponge $HISTFILE
-
 # don't put duplicate lines in the history and erase those that already exist
 # don't overwrite history on login, append to it.
 HISTCONTROL=erasedups:ignorespace
 shopt -s histappend
 PROMPT_COMMAND="history -n; history -w; history -c; history -r; $PROMPT_COMMAND"
 
-# check the window size after each command and, if necessary,
-# update the values of LINES and COLUMNS.
-shopt -s checkwinsize
+# Enable bash completion
+if ! $(isLinux); then
+	source $(brew --prefix bash-completion)/etc/bash_completion
+else
+	source /etc/bash_completion
+fi
+
+# Add tab completions for SSH hostnames based on ~/.ssh/config
+[ -e "$HOME/.ssh/config" ] && complete -o "default" -o "nospace" -W "$(grep "^Host" ~/.ssh/config | grep -v "[?*]" | cut -d " " -f2- | tr ' ' '\n')" scp sftp ssh;
 
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
@@ -51,8 +57,11 @@ shopt -s checkwinsize
 # set color command prompt
 export PS1='\[\033[01;30m\]\h \[\033[01;34m\]\w\[\033[00m\]\n\$ '
 
+# Prepend git branch name on command prompt.
+if [ -r "$SELF/lib/git/prompt.sh" ]; then source "$SELF/lib/git/prompt.sh"; fi
+
 # set file colors
 eval `dircolors ~/.dir_colors`
 
-# Prepend git branch name on command prompt.
-if [ -r "$SELF/git/prompt.sh" ]; then source "$SELF/git/prompt.sh"; fi
+# If an alias file exists, load it.
+if [ -f ~/.alias ]; then source ~/.alias; fi
