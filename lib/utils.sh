ccbf27d472179c48e767562570731a8a1abd7dcc
Héctor Menéndez <etor@gik.mx>
2015-11-08 07:28:25 -0600

    Modified the installation script to use files and folders.

--- lib/utils.sh
+++ lib/utils.sh
@@ -18,9 +18,9 @@ function has {
 function dotfiles {
     # try first the unix way, then the bsd way. (brew might be already installed)
     {
-        find $1 -maxdepth 1 -type f -name "*.sh" -exec basename {} ".sh" 2> /dev/null \;
+        find $1 -maxdepth 1 -exec basename {} 2> /dev/null \;
     } || {
-        find $1 -depth 1 -type f -name "*.sh" -exec basename {} ".sh" 2> /dev/null \;
+        find $1 -depth 1 -exec basename {} 2> /dev/null \;
     }
 }
 
