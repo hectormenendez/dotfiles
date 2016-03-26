d1c8c9c7a527fc598ec6d2ac4be310997e115e47
Héctor Menéndez <etor@gik.mx>
2016-03-25 20:23:11 -0600

    Vim plug installation is now done only when not already present (untested)

--- lib/install.sh
+++ lib/install.sh
@@ -5,10 +5,10 @@ isDarwin && source "$DOTFILES_LIB/install-osx.sh"
 isArch   && source "$DOTFILES_LIB/install-arch.sh"
 
 # Common installation steps
-echo "Updating vim's plugin manager"
-rm -Rf ~/.vim/autoload/plug.vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+[ ! -f ~/.vim/autoload/plug.vim ] && \
+	echo "Updating vim's plugin manager" && \
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo "Enabling latest version of node"
 target=$(nvm ls-remote | tail -1 | xargs)
