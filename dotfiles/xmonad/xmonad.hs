cb384daf0d208f7bd891b56327a7aa5423ee5a27
Hector Menendez <etor.mx@gmail.com>
2022-12-27 20:48:16 -0600

    XMonad config now uses custom layouts!

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -1,23 +1,40 @@
-import Data.Map as Map
+import qualified Data.Map as Map
+import qualified System.IO(putStrLn)
 
-import XMonad as X
+import qualified XMonad as X
 
-import XMonad.StackSet as StackSet
+import qualified XMonad.StackSet as StackSet
 
-import XMonad.Util.EZConfig as EZConfig
-import XMonad.Util.Run(spawnPipe)
+import qualified XMonad.Layout.ToggleLayouts as ToggleLayouts
+import qualified XMonad.Layout.ThreeColumns as Layout3Col
+
+import XMonad.Util.EZConfig(removeKeysP,additionalKeysP)
 import XMonad.Util.SpawnOnce(spawnOnce)
 
+import qualified XMonad.Hooks.EwmhDesktops as Desktops
 import XMonad.Hooks.ManageDocks(avoidStruts,manageDocks)
 import XMonad.Hooks.ManageHelpers(doRectFloat)
-import XMonad.Hooks.SetWMName
-import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.SetWMName(setWMName)
 
-import XMonad.Actions.GridSelect as GridSelect
-import XMonad.Actions.Minimize as Minimize
+myLayout =
+    avoidStruts $
+    ToggleLayouts.toggleLayouts layoutFull -- sets the default layout to full
+    (
+        layoutTiled X.|||
+        layout3Col X.|||
+        layoutFull
+    ) where
+        layoutFull = X.Full
+        layoutTiled = X.Tall 1 (3/100) (1/2)
+        layout3Col = Layout3Col.ThreeCol 1 (3/100) (1/2)
+
+-- myLayout = ThreeColMid 1 (3/100) (1/2) ||| master tall ||| Full
+--     where
+--         tall = Tall 1 (3/100) (1/2)
+--         master = masterWindowGaps tall
 
 -- NOTE: ewmhFullscreen: Fixes an issue with videos in Google Chrome
-main = xmonad $ ewmhFullscreen . ewmh $ def
+main = X.xmonad $ Desktops.ewmhFullscreen . Desktops.ewmh $ X.def
     {
         -- Run these on startup
         X.startupHook = do
@@ -29,11 +46,9 @@ main = xmonad $ ewmhFullscreen . ewmh $ def
             spawnOnce "nitrogen --restore &"
             -- Widgets and niceties
             spawnOnce "conky -c $HOME/.config/conky/default.conkyrc",
-        -- Wrap layouts so there's automatic spacing for docks,panels, trays
-        X.layoutHook = avoidStruts $ X.layoutHook def,
-        X.manageHook = composeAll [
-            className =? "1Password" --> doRectFloat (StackSet.RationalRect 0.25 0.25 0.5 0.5),
-            X.manageHook def <+> manageDocks
+        X.manageHook = X.composeAll [
+            X.className X.=? "1Password" X.--> doRectFloat (StackSet.RationalRect 0.25 0.25 0.5 0.5),
+            X.manageHook X.def X.<+> manageDocks
         ],
         -- How many workspaces?
         X.workspaces = ["1", "2", "3", "4", "5", "6"],
@@ -44,45 +59,67 @@ main = xmonad $ ewmhFullscreen . ewmh $ def
         X.normalBorderColor = "#000000",
         X.focusedBorderColor = "#666666",
         -- Use super as Modifier key
-        X.modMask = mod4Mask,
+        X.modMask = X.mod4Mask,
         -- Automatically focus window by passing the mouse over
-        X.focusFollowsMouse = True
+        X.focusFollowsMouse = True,
+        -- Wrap layouts so there's automatic spacing for docks,panels, trays
+        X.layoutHook = myLayout
     }
-    `EZConfig.removeKeysP` [
-        -- start additionalKeys
-        "M-<Space>",
+
+    `removeKeysP` [
         "M-q",
         "M-<Shift>-q",
-        "M-`",
-        "M-<Shift>-r",
-        "M-g",
+        "M-<Space>",
         "M-<Backspace>",
-        "M-m",
-        "M-<Shift>-m",
-        -- end additionalKeys
-        "M-p", -- used for VSCode
-        "M-S-p", -- used for VSCode
-        "M-,", -- used for Settings TODO: findout what this key did
-        "M-w", -- used for closing tabs TODO: findout whats this key did
-        "M-S-w" -- used for reopening tabs TODO: findout whats this key did
+        "<Print>",
+        "M-q",
+        "M-`"
     ]
-    `EZConfig.additionalKeysP` [
-        -- use the custom-built dmenu_run on ~/Source/AUR/dmenu
-        ( "M-<Space>", spawn "rofi -show combi" ),
-        ( "M-q", kill ),
-        ( "M-<Shift>-q", spawn "killall xmonad" ),
-        ( "M-`", spawn "xset dpms force off"), -- turn off screen
-        ( "M-<Shift>-r", spawn "xmonad --recompile; xmonad --restart" ),
-        -- TODO: fix this
-        -- ( "M-g", GridSelect.goToSelected GridSelect.defaultGSConfig ),
-        ( "M-<Backspace>", withFocused _toggleFloat),
-        ( "M-m", withFocused minimizeWindow ),
-        ( "M-<Shift>-m", Minimize.withLastMinimized Minimize.maximizeWindowAndFocus ),
-        ( "M-<Alt>-<Ctrl>-p", spawn "1password"),
-        ( "<Print>", spawn "scrot --select '%Y-%m-%d_$wx$h.png' -e 'mv $f ~/Downloads/'")
+
+    `additionalKeysP` [
+        ( "M-q", X.kill ),
+        ( "M-<Shift>-q", X.spawn "killall xmonad" ),
+        ( "M-<Space>", X.spawn "rofi -show combi" ),
+        ( "M-<Backspace>", X.withFocused toggleFloat),
+        ( "<Print>", X.spawn "scrot --select '%Y-%m-%d_$wx$h.png' -e 'mv $f ~/Downloads/'"),
+        ( "M-`", X.sendMessage ToggleLayouts.ToggleLayout)
     ]
+
     where
-        _toggleFloat w = windows (\s ->
-            if Map.member w (StackSet.floating s)
-            then StackSet.sink w s
-            else StackSet.float w (StackSet.RationalRect 0.25 0.25 0.5 0.5) s)
+        toggleFloat win = X.windows (\s ->
+            if Map.member win (StackSet.floating s)
+            then StackSet.sink win s
+            else StackSet.float win (StackSet.RationalRect 0.25 0.25 0.5 0.5) s)
+
+    -- X.layoutHook X.def
+
+
+-- myLayout =
+-- 	avoidStruts $
+-- 	smartBorders $
+-- 	toggleLayouts Full
+-- 	(tiled ||| ThreeColMid 1 (3/100) (1/3) ||| Mirror tiled ||| tabbedLayout ||| gimpLayout)
+-- 	where
+-- 		tiled = Tall nmaster delta ratio
+-- 		nmaster = 1
+-- 		delta = 3/100
+-- 		ratio = toRational(2/(1+sqrt(5)::Double))
+
+-- 		gimpLayout = named "Gimp" (
+-- 			withIM (0.15) (Role "gimp-toolbox") $
+-- 			reflectHoriz $
+-- 			withIM (0.2) (Role "gimp-dock") Full
+-- 			)
+-- 		tabbedLayout = named "Tabbed" (tabbedBottom shrinkText myTabConfig)
+-- 		myTabConfig = defaultTheme
+-- 			{ activeColor = "#1a1a1a"
+-- 			, inactiveColor = "#000000"
+-- 			, urgentColor = "#1a1a1a"
+-- 			, activeTextColor = "#00ffff"
+-- 			, inactiveTextColor = "#ffbe33"
+-- 			, urgentTextColor = "#ff00ff"
+-- 			, activeBorderColor = "#000000"
+-- 			, inactiveBorderColor = "#1a1a1a"
+-- 			, urgentBorderColor = "#000000"
+-- 			}
+
