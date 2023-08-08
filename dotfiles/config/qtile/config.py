30756a30e08fe9780c633dad0a09ca359c8ef38c
Hector Menendez <etor.mx@gmail.com>
2023-08-07 18:34:51 -0600

    arch: qtile: working config (not used because glitch while dragging floats)

--- dotfiles/config/qtile/config.py
+++ dotfiles/config/qtile/config.py
@@ -5,6 +5,13 @@ from libqtile.lazy import lazy
 from libqtile.config import Key, Rule, Match
 from libqtile import hook
 
+# -- local imports
+from graphical_notifications import Notifier
+
+# plugin that acts as notification server and draws notification windows
+# there are keybindings to handle the notifications, but I haven't enabled them.
+notifier = Notifier()
+
 # run these commands at startup (not on restarts)
 @hook.subscribe.startup_once
 def autostart():
@@ -18,8 +25,7 @@ mod = "mod4"
 wmname = "LG3D"
 
 # apps cannot go fullscreen themselves
-# TODO: Test with youtube
-auto_fullscreen = False
+auto_fullscreen = True
 
 # apps cannot minimize themselves
 auto_minimize = False
@@ -40,10 +46,44 @@ keys = [
     # now we have the basics covered
     Key([mod, "shift"], "Return", lazy.spawn("kitty"), desc="Terminal"),
 
-    # --- switching apps
+    # --- managing apps
 
+    Key([mod], "q", lazy.window.kill(), desc="Quit Current App"),
     Key([mod], "space", lazy.spawn("rofi -show combi -show calc -show emoji -show filebrowser")), 
     Key([mod], "tab", lazy.spawn("rofi -show window")),
     Key([mod, "shift"], "space", lazy.spawn("rofimoji")),
+
+    # --- managing layout
+    Key([mod], "escape", lazy.window.toggle_floating(), desc="Toggle Floating"),
+
+    Key([mod], "l", lazy.layout.right()),
+    Key([mod], "h", lazy.layout.left()),
+    Key([mod], "k", lazy.layout.up()),
+    Key([mod], "j", lazy.layout.down()),
+
+    Key([mod, "shift"], "l",
+        lazy.layout.grow_right(),
+        lazy.layout.grow(),
+        lazy.layout.increase_ratio(),
+        lazy.layout.delete(),
+    ),
+    Key([mod, "shift"], "h",
+        lazy.layout.grow_left(),
+        lazy.layout.shrink(),
+        lazy.layout.decrease_ratio(),
+        lazy.layout.add(),
+    ),
+    Key([mod, "shift"], "k",
+        lazy.layout.grow_up(),
+        lazy.layout.grow(),
+        lazy.layout.decrease_nmaster(),
+    ),
+    Key([mod, "shift"], "j",
+        lazy.layout.grow_down(),
+        lazy.layout.shrink(),
+        lazy.layout.increase_nmaster(),
+    ),
+
+    # --- managing computer
 ]
 
